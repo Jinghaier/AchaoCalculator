21f0d2307e94a053373e7b9214a247f40772c3f2
Jinghaier <1154597325@qq.com>
2019-03-28 17:29:25 +0800

    a

---
+++ Jinghaier/Calculator/Calculator/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要出题的个数：");
+            int n = Convert.ToInt32(Console.ReadLine());
+            PrintCalculator(n);
+            Console.ReadKey();
+        }
+        public static void PrintCalculator(int n)
+        {
+            Random rd = new Random();
+            int a, b, c, d, e, r;
+            int i = 0;
+            string str;
+            do
+            {
+                a = rd.Next(0, 100);
+                b = rd.Next(0, 100);
+                c = rd.Next(0, 100);
+                d = rd.Next(0, 100);
+                e = rd.Next(1, 100);
+
+                string t = Convert.ToString(rd.Next(7));
+                switch (t)
+                {
+                    case "0":
+                        r = a + b - c;
+                        str = a + " + " + b + " - " + c + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+
+                        break;
+                    case "1":
+                        r = a - b + c;
+                        str = a + " - " + b + " + " + c + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                           
+                            i++;
+                        }
+
+                        break;
+                    case "2":
+                        r = a / e - c;
+                        str = a + " / " + e + " - " + c + " = " + r;
+                        if (a % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+                          
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "3":
+                        r = a / e * c;
+                        str = a + " / " + e + " * " + c + " = " + r;
+                        if (a % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "4":
+                        r = a * b - c + d;
+                        str = a + " * " + b + " - " + c + " + " + d + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                         
+                            i++;
+                        }
+
+                        break;
+                    case "5":
+                        r = a * b - c / e;
+                        str = a + " * " + b + " - " + c + " / " + e + " = " + r;
+                        if (c % e == 0 && r >= 0)
+                        {
+                            Console.WriteLine(str);
+                      
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        break;
+                    case "6":
+                        r = a * b * c + d;
+                        str = a + " * " + b + " * " + c + " + " + d + " = " + r;
+                        if (r >= 0)
+                        {
+                            Console.WriteLine(str);
+                          
+                            i++;
+                        }
+
+                        break;
+                }
+            } while (i < n);
+        }
+
+    }
+}
---
+++ Jinghaier/Calculator/CalculatorUnitTestProject3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUnitTestProject3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUnitTestProject3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68fd57aa-5374-4fa4-aba3-976de2743597")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
